8717d6b75a7fe7dd68ad330f0ad8e82942520a14
Sam Glover <sam@samglover.net>
2015-11-18 21:26:53 -0600

    Correct stylesheet references.

--- wp-editor-tweaks.php
+++ wp-editor-tweaks.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
 Plugin Name: WP Editor Tweaks
 Plugin URI: http://wordpress.org/plugins/wp-editor-tweaks/
@@ -7,22 +8,30 @@ Author: Sam Glover
 Version: 1.3.1
 Author URI: http://samglover.net
 */
+
 /*--------------------------------------------------
 Add Stylesheet for the WordPress Text Editor
 --------------------------------------------------*/
+
 function custom_text_editor_styles() {
-  wp_enqueue_style('editor-styles', plugins_url('text-editor-styles.css', __FILE__));
+  wp_enqueue_style('editor-styles', plugins_url('wpet-text-editor-styles.css', __FILE__));
 }
+
 add_action('admin_enqueue_scripts', 'custom_text_editor_styles');
 add_action('login_enqueue_scripts', 'custom_text_editor_styles');
+
+
 /*--------------------------------------------------
 Add Stylesheet for the TinyMCE/Visual Editor
 --------------------------------------------------*/
+
 function plugin_mce_css( $mce_css ) {
 	if ( ! empty( $mce_css ) )
 		$mce_css .= ',';
-	$mce_css .= plugins_url( 'tinymce-editor-styles.css', __FILE__ );
+	$mce_css .= plugins_url( 'wpet-tinymce-editor-styles.css', __FILE__ );
 	return $mce_css;
 }
+
 add_filter( 'mce_css', 'plugin_mce_css' );
+
 ?>
